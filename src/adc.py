7261987cb01539c5ae9ecdc840d08c17a39c15b9
Jarosław Komar <jaszczur666@o2.pl>
2024-01-09 11:14:57 +0100

    Added some comments

--- src/adc.py
+++ src/adc.py
@@ -14,8 +14,10 @@ class ADC:
 
     def get_value(self):
         serial_string = b""  # Used to hold data coming over UART
-        while serial_string == b'':
-            self.serialPort.write(b"test\n")
+        while serial_string == b'':  # Sometimes readLine will give empty string, this is not acceptable, so we query
+            # ADC until it gives valid answer
+            self.serialPort.write(b"test\n")  # This actually could be even bare "\n", but experiment showed that
+            # sometimes this would break. Sending any short string before \n seems to prevent this.
             self.serialPort.flush()
             serial_string = self.serialPort.readline()
         return int(serial_string)
