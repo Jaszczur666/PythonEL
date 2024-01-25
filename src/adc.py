34fd9bd99e8be62751fc1ff25e5bb510f4db1d4f
Jarosław Komar <jaszczur666@o2.pl>
2024-01-25 21:43:02 +0100

    Removed unnecessary workaround

--- src/adc.py
+++ src/adc.py
@@ -7,27 +7,24 @@ class ADC:
     def __init__(self):
         self.serialPort = serial.Serial()
 
-    def init(self, port_name="COM9"):
+    def init(self, port_name="COM11"):
         self.serialPort = serial.Serial(
             port=port_name,
             baudrate=9600,
-            bytesize=8,
-            timeout=2,
+            bytesize=serial.EIGHTBITS,
+            timeout=None,
             stopbits=serial.STOPBITS_ONE,
         )
+        time.sleep(3) # Give arduino that serves as intermediate time to reset
 
     def get_value(self):
         serial_string = b""  # Used to hold data coming over UART
-        while (
-            serial_string == b""
-        ):  # Sometimes readLine will give empty string, this is not acceptable, so we query
-            # ADC until it gives valid answer
-            self.serialPort.write(
-                b"test\n"
-            )  # This actually could be even bare "\n", but experiment showed that
-            # sometimes this would break. Sending any short string before \n seems to prevent this.
-            self.serialPort.flush()
-            serial_string = self.serialPort.readline()
+        self.serialPort.write(
+            b"test\n"
+        )  # This actually could be even bare "\n", but experiment showed that
+        # sometimes this would break. Sending any short string before \n seems to prevent this.
+        self.serialPort.flush()
+        serial_string = self.serialPort.readline()
         return int(serial_string)
 
     def close_port(self):
